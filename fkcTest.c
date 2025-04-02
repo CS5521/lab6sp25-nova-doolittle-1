1ce25ff357a7979d7e5ba5d39de0646cef5d5d18
DoolittleNova <doolittletr@appstate.edu>
2025-04-02 21:47:19 +0000

    finished lab I think

--- fkcTest.c
+++ fkcTest.c
@@ -5,23 +5,35 @@
 int
 main(int argc, char * argv[])
 {
+    int i;
+    int pid;
 
     printf(0, "number of forks when program starts: %d\n", fkc(1));
-    int pid = fork();
-    if (pid == 0) {
-        exit();
+    
+    for (i = 0; i < 2; i++)
+    {
+        pid = fork();
+        if (pid == 0) exit();
     }
-    pid = fork();
-    if (pid == 0) {
-        exit();
+
+    for (i = 0; i < 2; i++) {
+        wait();
     }
-    wait();
-    wait();
+
     printf(0, "number of forks after two more forks: %d\n", fkc(1));
     
-    //printf(0, "number of forks after two more forks: %d\n", fkc(1));
-    //printf(0, "number of forks after fork count cleared: %d\n", fkc(0));
+    printf(0, "number of forks after fork count cleared: %d\n", fkc(0));
+    
+    for (i = 0; i < 3; i++)
+    {
+        pid = fork();
+        if (pid == 0) exit();
+    }
 
-    //printf(0, "number of forks after three more forks: %d\n", fkc(1));
+    for (i = 0; i < 3; i++) {
+        wait();
+    }
+
+    printf(0, "number of forks after three more forks: %d\n", fkc(1));
     exit();
 }
